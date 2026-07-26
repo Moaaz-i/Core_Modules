45a3c16d0d789efe741d10f1765ef4ba10c7415a
Moaaz-i <famile.sherif@gmail.com>
2026-07-26 06:21:12 +0300

    refactor: import TaskPulse from published cronflex npm package

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { TaskPulse } from "../CronFlex/src/index.js";
+import { TaskPulse } from "cronflex";
 import * as assignment from "./assignment2.js";
 import { findKthPositive } from "./bonus.js";
 
